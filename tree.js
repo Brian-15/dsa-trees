539e5a31af8789b5cd9936fcf9081fc1531da7b8
Brian-15 <bcirieco@gmail.com>
2022-01-31 21:16:24 -0800

    implemented Tree class methods

--- tree.js
+++ tree.js
@@ -14,21 +14,46 @@ class Tree {
 
   /** sumValues(): add up all of the values in the tree. */
 
-  sumValues() {
-    
+  sumValues(parent=this.root) {
+    if (this.root === null) return 0;
+    let sum = parent.val;
+    for (let node of parent.children) {
+      if (node.children.length > 0) {
+        sum += this.sumValues(node);
+      } else {
+        sum += node.val;
+      }
+    }
+    return sum;
   }
 
   /** countEvens(): count all of the nodes in the tree with even values. */
 
-  countEvens() {
-
+  countEvens(parent=this.root) {
+    if (this.root === null) return 0;
+    let count = parent.val % 2 === 0 ? 1 : 0;
+    for (let node of parent.children) {
+      if (node.children.length > 0) count += this.countEvens(node);
+      else node.val % 2 === 0 ? count++ : undefined;
+    }
+    return count;
   }
 
   /** numGreater(lowerBound): return a count of the number of nodes
    * whose value is greater than lowerBound. */
 
-  numGreater(lowerBound) {
+  numGreater(lowerBound, parent=this.root) {
+    if (this.root === null) return 0;
 
+    let count = parent.val > lowerBound ? 1 : 0;
+    for (let node of parent.children) {
+      if (node.children.length > 0) {
+        count += this.numGreater(lowerBound, node);
+      } else {
+        node.val > lowerBound ? count++ : undefined;
+      }
+    }
+    return count;
   }
 }
 
